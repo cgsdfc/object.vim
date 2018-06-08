22340eb4db0ea28f40146caff1056f7c783e918c
cgsdfc <cgsdfc@126.com>
2018-06-08 19:18:14 +0800

    Fix rtp

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,4 +6,4 @@ set -p
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
 : "${VADER_TEST_VIM:=vim}"
-eval "$VADER_TEST_VIM -Nu vimrc -c 'Vader! *'"
+eval "$VADER_TEST_VIM -Nu vimrc -c 'Vader! object.vader'"
